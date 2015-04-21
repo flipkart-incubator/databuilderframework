cadf41da3634df3911c69eb57635d8e44242e49e
gaurav.prasad <gaurav.prasad@flipkart.com>
2015-04-21 14:51:32 +0530

    Removing mdeploy

--- mdeploy-release.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean source:jar deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-release-local
-mvn clean source:jar deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local
--- mdeploy-snapshots.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-mvn clean source:jar deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-snapshot-local
-mvn clean source:jar deploy -DskipTests -DaltDeploymentRepository=flipkart::default::http://artifactory.nm.flipkart.com:8081/artifactory/libs-snapshots-local
