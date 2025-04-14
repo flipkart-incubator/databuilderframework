e699b519ff86af251ece884d7a02b56f6c68d65c
Rishabh Goyal <rishabh@phonepe.com>
2025-04-14 15:17:20 +0530

    Added Jacoco

--- src/main/java/com/flipkart/databuilderframework/engine/Utils.java
+++ src/main/java/com/flipkart/databuilderframework/engine/Utils.java
@@ -30,8 +30,8 @@ public class Utils {
     }
 
     public static String name(Class<?> clazz) {
-        return CLASS_TO_NAME_MAPPING.computeIfAbsent(clazz,
-                aClass -> CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, clazz.getSimpleName()));
+        return CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE,
+                CLASS_TO_NAME_MAPPING.computeIfAbsent(clazz, aClass -> clazz.getSimpleName()));
     }
 
     public static boolean isEmpty(Collection collection) {
